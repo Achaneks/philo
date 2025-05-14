f7941a98f1ae2a98d3c663c14c53f8b81c03dbb2
Anas Chanek <achanek@c2r8p3.1337.ma>
2025-05-14 16:37:40 +0100

    the first push

---
+++ include/philo.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: achanek <achanek@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/13 16:34:04 by achanek           #+#    #+#             */
+/*   Updated: 2025/04/23 11:12:17 by achanek          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+typedef struct s_philo
+{
+	int number_of_philosophers;
+	int time_to_die;
+	int time_to_eat;
+	int time_to_sleep;
+	int number_of_times_each_philosopher_must_eat;
+} t_philo;
+
+
+
+# endif
